e236d7e7cf79314c319f3924e3773a04690a16a5
nickinchina <shoutsoft@hotmail.com>
2013-12-27 00:13:14 +0800

    xxxx

--- src/easypos/qb/PostHelper.java
+++ src/easypos/qb/PostHelper.java
@@ -13,19 +13,32 @@ public class PostHelper
 	    	JSONTokener jsonParser = new JSONTokener(JSON);    
 		    JSONObject je = (JSONObject) jsonParser.nextValue();  
 		    JSONArray lines = je.getJSONArray("lines");  
-		    
+
 		    Journalentry qje = new Journalentry();
+		    String refid = je.getString("refid");
+		    if (refid != "")
+		    {
+		    	qje.get(refid);
+		    	qje.delete();
+		    }
+		    qje.reset();
 			qje.setRefNumber(je.getString("ref"));
 			qje.setTransactionDate(je.getString("date"));
 			int i;
 			for (i=0; i<lines.length(); i++) {
 				JSONObject jeline = lines.getJSONObject(i);
 			    JournalLine qjl = new inqb.JournalLine();
-			    qjl.setAccountName(jeline.getString("account"));
+			    String act = jeline.getString("account");
+			    this.AddAccount(act, jeline.getInt("atype"));
+			    qjl.setAccountName(act);
 			    qjl.setMemo(jeline.getString("memo"));
 			    qjl.setClassName(jeline.getString("class"));
 			    qjl.setLineType(jeline.getInt("dir"));
 			    qjl.setAmount(jeline.getString("amt"));
+			    String ent = jeline.getString("ent");
+			    if (ent != "")
+			    	qjl.setEntityName(ent);
+			    
 			    qje.getJournalLines().add(qjl);
 			}
 			qje.add();
@@ -36,4 +49,25 @@ public class PostHelper
 			return e.toString();
 		}
 	}
+	
+	private void AddAccount(String name, int accountType)
+	{
+		Account a = new Account();
+		try {
+			a.getByName(name);
+		}
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			try {
+				a.reset();
+				a.setAccountName(name);
+				a.setAccountType(accountType);
+				a.add();
+			} catch (InQBException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+	
 }
